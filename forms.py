b988ec0f29b669d0cdf6630fbe296ae34d3e937c
Susmitha5c3 <lakshmisusmitha13@gmail.com>
2020-05-05 19:27:00 +0530

    Implemented TODOs

--- forms.py
+++ forms.py
@@ -10,27 +10,27 @@ def validate_phone(form, field):
         raise ValidationError("Phone number should only contain digits.")
 
 
-# genres_choices = [
-#     ('Alternative', 'Alternative'),
-#     ('Blues', 'Blues'),
-#     ('Classical', 'Classical'),
-#     ('Country', 'Country'),
-#     ('Electronic', 'Electronic'),
-#     ('Folk', 'Folk'),
-#     ('Funk', 'Funk'),
-#     ('Hip-Hop', 'Hip-Hop'),
-#     ('Heavy Metal', 'Heavy Metal'),
-#     ('Instrumental', 'Instrumental'),
-#     ('Jazz', 'Jazz'),
-#     ('Musical Theatre', 'Musical Theatre'),
-#     ('Pop', 'Pop'),
-#     ('Punk', 'Punk'),
-#     ('R&B', 'R&B'),
-#     ('Reggae', 'Reggae'),
-#     ('Rock n Roll', 'Rock n Roll'),
-#     ('Soul', 'Soul'),
-#     ('Other', 'Other'),
-# ]
+genres_choices = [
+    ('Alternative', 'Alternative'),
+    ('Blues', 'Blues'),
+    ('Classical', 'Classical'),
+    ('Country', 'Country'),
+    ('Electronic', 'Electronic'),
+    ('Folk', 'Folk'),
+    ('Funk', 'Funk'),
+    ('Hip-Hop', 'Hip-Hop'),
+    ('Heavy Metal', 'Heavy Metal'),
+    ('Instrumental', 'Instrumental'),
+    ('Jazz', 'Jazz'),
+    ('Musical Theatre', 'Musical Theatre'),
+    ('Pop', 'Pop'),
+    ('Punk', 'Punk'),
+    ('R&B', 'R&B'),
+    ('Reggae', 'Reggae'),
+    ('Rock n Roll', 'Rock n Roll'),
+    ('Soul', 'Soul'),
+    ('Other', 'Other'),
+]
 state_choices = [
     ('AL', 'AL'),
     ('AK', 'AK'),
