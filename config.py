7cd36373897840740c3f1186ade7748902397133
Susmitha5c3 <lakshmisusmitha13@gmail.com>
2020-05-05 19:24:37 +0530

    Connection to databse

--- config.py
+++ config.py
@@ -8,6 +8,5 @@ DEBUG = True
 
 # Connect to the database
 
-
-# TODO IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = 'postgres://udacity:susmitha@localhost:5432/fyyur'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
